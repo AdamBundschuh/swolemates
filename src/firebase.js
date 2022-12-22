c9a2e0c1c65229ee31e040aa984babd9fdfd4640
Adam Bundschuh <bundschuh.adam@gmail.com>
2022-12-22 14:03:37 -0500

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA8YQY7r27dJa7jaBwlu06guTTinTTYPKk",
+    authDomain: "swolemates-68890.firebaseapp.com",
+    projectId: "swolemates-68890",
+    storageBucket: "swolemates-68890.appspot.com",
+    messagingSenderId: "630391635433",
+    appId: "1:630391635433:web:7deaf6532e0de388491f78",
+    measurementId: "G-NS5YF9Z1ZP"
+  };
