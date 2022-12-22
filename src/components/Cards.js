4514da0001af9b7a70d46bf7a518a6c01fca8202
Adam Bundschuh <bundschuh.adam@gmail.com>
2022-12-22 14:44:14 -0500

    Connect to firebase db

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,22 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import "../styling/Cards.css";
+import database from '../firebase.js';
 
 function Cards() {
-  const [people, setPeople] = useState([
-    {
-      name: "Chris Evans",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/33/Mark_Kassen%2C_Tony_C%C3%A1rdenas_and_Chris_Evans_%28cropped%29.jpg",
-    },
-    {
-      name: "Chris Hemsworth",
-      url: "https://upload.wikimedia.org/wikipedia/commons/e/e8/Chris_Hemsworth_by_Gage_Skidmore_2_%28cropped%29.jpg",
-    },
-    {
-        name: "Chris Pratt",
-        url: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2022%2F06%2F27%2FChris-Pratt_01.jpg",
-      },
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+       database
+       .collection('people')
+       .onSnapshot(snapshot => (
+        setPeople(snapshot.docs.map(doc => doc.data()))
+       )); 
+  }, []);
 
   return (
     <div>
